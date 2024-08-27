7026c4292903ea33c1ae68bddcdb7cf64b9137c4
TheSizar <cezary.lastowski@gmail.com>
2024-08-27 23:27:42 +0200

    Groq chat added

---
+++ scripts/tests/test_ssl.py
@@ -0,0 +1,9 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Disable SSL verification warning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Make the request without SSL verification
+response = requests.get('https://api.groq.com', verify=False)
+print(response.status_code)
